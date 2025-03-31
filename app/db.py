9c0fd2e918425dda0f5177ac91c4e7920d9223b8
bruhtesfa-gh <bruhtesfazelealem436@gmail.com>
2025-03-31 03:27:13 +0300

    Update DATABASE_URL handling in db.py to use a default in-memory SQLite database for test mode, enhancing flexibility in database management.

--- app/db.py
+++ app/db.py
@@ -6,9 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from app.util.setting import get_settings
 
 settings = get_settings()
-DATABASE_URL = (
-    settings.DATABASE_URL_TEST if settings.TEST_MODE else settings.DATABASE_URL
-)
+DATABASE_URL = settings.DATABASE_URL | "sqlite+aiosqlite:///:memory:"
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 
