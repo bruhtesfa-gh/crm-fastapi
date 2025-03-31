41a3c704e3bd389ede13aaf04aa3e102cdebab86
bruhtesfa-gh <bruhtesfazelealem436@gmail.com>
2025-03-31 04:15:22 +0300

    Refactor test_user.py to replace hardcoded admin token headers with a parameterized approach, enhancing maintainability and consistency across user-related tests.

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -3,16 +3,12 @@ from typing import Dict
 import pytest
 from httpx import AsyncClient
 
-_admin_token_headers = {
-    "Authorization": f"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDU5NTk4NDQuMDI4NywibmJmIjoxNzQzMzY3ODQ0LCJzdWIiOiIxIiwidXNlciI6eyJjcmVhdGVkX2F0IjoiMjAyNS0wMy0zMFQxODo0OTo1NS43OTA1NzUiLCJ1cGRhdGVkX2F0IjoiMjAyNS0wMy0zMFQxODo0OTo1NS43OTA1NzUiLCJpZCI6MSwidXNlcm5hbWUiOiJhZG1pbkBnbWFpbC5jb20iLCJyb2xlX2lkIjoxLCJyb2xlIjp7ImlkIjoxLCJuYW1lIjoiQWRtaW4iLCJkZXNjcmlwdGlvbiI6IkFkbWluIHdpdGggcGVybWlzc2lvbiB0byBtYW5hZ2UgdXNlcnMsIHJvbGVzLCBhbmQgYXVkaXQgbG9ncy4iLCJwZXJtaXNzaW9ucyI6W3siaWQiOjIsIm5hbWUiOiJHRVQ6L3VzZXJzL21lLyIsImRlc2NyaXB0aW9uIjoiUGVybWlzc2lvbiB0byBHRVQ6L3VzZXJzL21lLyJ9LHsiaWQiOjMsIm5hbWUiOiJHRVQ6L3VzZXJzLyIsImRlc2NyaXB0aW9uIjoiUGVybWlzc2lvbiB0byBHRVQ6L3VzZXJzLyJ9LHsiaWQiOjQsIm5hbWUiOiJHRVQ6L3VzZXJzLyovIiwiZGVzY3JpcHRpb24iOiJQZXJtaXNzaW9uIHRvIEdFVDovdXNlcnMvKi8ifSx7ImlkIjo1LCJuYW1lIjoiUFVUOi91c2Vycy8qLyIsImRlc2NyaXB0aW9uIjoiUGVybWlzc2lvbiB0byBQVVQ6L3VzZXJzLyovIn0seyJpZCI6NiwibmFtZSI6IkRFTEVURTovdXNlcnMvKi8iLCJkZXNjcmlwdGlvbiI6IlBlcm1pc3Npb24gdG8gREVMRVRFOi91c2Vycy8qLyJ9LHsiaWQiOjcsIm5hbWUiOiJQVVQ6L3VzZXJzLyovcm9sZS8iLCJkZXNjcmlwdGlvbiI6IlBlcm1pc3Npb24gdG8gUFVUOi91c2Vycy8qL3JvbGUvIn0seyJpZCI6OCwibmFtZSI6IkdFVDovcm9sZXMvIiwiZGVzY3JpcHRpb24iOiJQZXJtaXNzaW9uIHRvIEdFVDovcm9sZXMvIn0seyJpZCI6OSwibmFtZSI6IlBPU1Q6L3JvbGVzLyIsImRlc2NyaXB0aW9uIjoiUGVybWlzc2lvbiB0byBQT1NUOi9yb2xlcy8ifSx7ImlkIjoxMCwibmFtZSI6IkdFVDovcm9sZXMvKi8iLCJkZXNjcmlwdGlvbiI6IlBlcm1pc3Npb24gdG8gR0VUOi9yb2xlcy8qLyJ9LHsiaWQiOjExLCJuYW1lIjoiUFVUOi9yb2xlcy8qLyIsImRlc2NyaXB0aW9uIjoiUGVybWlzc2lvbiB0byBQVVQ6L3JvbGVzLyovIn0seyJpZCI6MTIsIm5hbWUiOiJERUxFVEU6L3JvbGVzLyovIiwiZGVzY3JpcHRpb24iOiJQZXJtaXNzaW9uIHRvIERFTEVURTovcm9sZXMvKi8ifSx7ImlkIjoxMywibmFtZSI6IlBPU1Q6L3JvbGVzLyovcGVybWlzc2lvbnMvKi8iLCJkZXNjcmlwdGlvbiI6IlBlcm1pc3Npb24gdG8gUE9TVDovcm9sZXMvKi9wZXJtaXNzaW9ucy8qLyJ9LHsiaWQiOjE0LCJuYW1lIjoiREVMRVRFOi9yb2xlcy8qL3Blcm1pc3Npb25zLyovIiwiZGVzY3JpcHRpb24iOiJQZXJtaXNzaW9uIHRvIERFTEVURTovcm9sZXMvKi9wZXJtaXNzaW9ucy8qLyJ9XX19LCJ0eXBlIjoiYXBpIiwiYWN0aW9uIjoiYWNjZXNzIn0.Un09M0Z2FBks7U6dSjpMCSH4JmfXosiMDL9slzfKER4"
-}
-
 
 @pytest.mark.asyncio
 async def test_get_users(
     client: AsyncClient, admin_token_headers: Dict[str, str]
 ) -> None:
-    r = await client.get("/users/me", headers=_admin_token_headers)
+    r = await client.get("/users/me", headers=admin_token_headers)
     assert r.status_code == 200
     assert r.json()["username"] == "admin@gmail.com"
 
@@ -20,7 +16,7 @@ async def test_get_users(
 async def test_get_user_by_id(
     client: AsyncClient, admin_token_headers: Dict[str, str]
 ) -> None:
-    r = await client.get("/users/1", headers=_admin_token_headers)
+    r = await client.get("/users/1", headers=admin_token_headers)
     assert r.status_code == 200
     assert r.json()["username"] == "admin@gmail.com"
 
@@ -28,7 +24,7 @@ async def test_get_user_by_id(
 async def test_get_user_by_id_not_found(
     client: AsyncClient, admin_token_headers: Dict[str, str]
 ) -> None:
-    r = await client.get("/users/999", headers=_admin_token_headers)
+    r = await client.get("/users/999", headers=admin_token_headers)
     assert r.status_code == 404
 
 
@@ -37,7 +33,7 @@ async def test_update_user(
 ) -> None:
     r = await client.put(
         "/users/2",
-        headers=_admin_token_headers,
+        headers=admin_token_headers,
         json={"username": "testuser@gmail.com"},
     )
     assert r.status_code == 403
@@ -48,7 +44,7 @@ async def test_update_user_not_found(
 ) -> None:
     r = await client.put(
         "/users/999",
-        headers=_admin_token_headers,
+        headers=admin_token_headers,
         json={"username": "testuser@gmail.com"},
     )
     assert r.status_code == 404
